f261e699d366c53b4ef03c95687f69d37bbde1cf
DESKTOP-BG25CL1\admin <van.la@paradiseio.com>
2023-12-31 17:51:03 +0700

    30p

--- src/components/form/SignInForm.tsx
+++ src/components/form/SignInForm.tsx
@@ -8,6 +8,7 @@ import { FormField, FormItem, FormLabel, FormControl, FormMessage } from "../ui/
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 
 const FormSchema = z.object({
@@ -57,8 +58,15 @@ const SignInForm = () => {
 
         </div>
         
-        <Button type="submit">Submit</Button>
+        <Button className="w-full mt-6"type="submit">Sign In</Button>
       </form>
+      <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+        or
+      </div>
+      <p className="text-center text-sm text-gray-600 mt-2">
+        If you don&apos;t have and account, please&nbsp;
+        <Link className="text-blue-500 hover:underline"href={"/sign-up"}>Sign Up</Link>
+      </p>
     </Form>
   )
 };
@@ -66,4 +74,3 @@ const SignInForm = () => {
 
 export default SignInForm;
 
-//tới 26p https://www.youtube.com/watch?v=GkpEWkKQego
